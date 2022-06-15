690c55f162e6ddc96527cbe89c8042019754bbdf
narakarax <liliyaogerenko@gmail.com>
2022-06-14 22:21:17 -0700

    complete

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,30 @@
+class CharactersController < ApplicationController
+    
+    
+    def index
+        url = "https://api.disneyapi.dev/characters"
+        response = RestClient.get(url)
+        render json: response
+    end
+
+    
+     def show
+            character = Character.find_by(id: params[:id])
+            render json: character, status: :ok
+    end
+
+    
+
+
+
+    
+  
+
+   
+
+    
+
+    
+    
+      
+end
